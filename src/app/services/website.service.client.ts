acf042d3f2c16f70f9efec91fb52887ef44dc47e
Shiyu Wang <shiyu3169@gmail.com>
2018-05-14 09:31:34 -0400

    website client service

---
+++ src/app/services/website.service.client.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+
+// injecting service into module
+@Injectable()
+
+export class WebsiteService {
+
+  constructor() { }
+	websites = [
+	  { _id: "123", name: "Facebook", developerId: "456", description: "Lorem" },
+	  { _id: "234", name: "Tweeter",  developerId: "456", description: "Lorem" },
+	  { _id: "456", name: "Gizmodo",   developerId: "456", description: "Lorem" },
+	  { _id: "890", name: "Go", developerId: "123", description: "Lorem" },
+	  { _id: "567", name: "Tic Tac Toe", developerId: "123", description: "Lorem" },
+	  { _id: "678", name: "Checkers", developerId: "123", description: "Lorem" },
+	  { _id: "789", name: "Chess", developerId: "234", description: "Lorem" }
+	];
+
+	createWebsite(userId, website) {
+		website._id = Math.floor(Math.random() * 10000).toString();
+		website.devloperId = userId;
+		this.websites.push(website);
+		return website;
+	}
+
+	findWebsitesByUser(userId){
+		var result = [];
+		for (let i = 0;i<this.websites.length;i++){
+			if (this.websites[i].developerId === userId) {
+				result.push(this.websites[i]);
+			}
+		}
+		return result;
+	}
+
+	findWebsiteById(websiteId) {
+		for (let i = 0;i<this.websites.length;i++){
+			if (this.websites[i]._id === websiteId) {
+				return this.websites[i];
+			}
+		}
+	}
+
+	updateWebsite(websiteId, website) {
+		var oldWeb = this.findWebsiteById(websiteId);
+		var index = this.websites.indexOf(oldWeb);
+		this.websites[index].name = website.name;
+		this.websites[index].description = website.description;
+	}
+
+	deleteWebsite(websiteId) {
+		var web = this.findWebsiteById(websiteId);
+		var index = this.websites.indexOf(web);
+		this.websites.splice(index, 1);
+	}
+
+}
