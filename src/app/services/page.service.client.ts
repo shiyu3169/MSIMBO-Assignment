259f9299a53f773cc114db6c34c9461f1d2fefe4
Shiyu Wang <shiyu3169@gmail.com>
2018-05-15 09:29:27 -0400

    page service client ts

---
+++ src/app/services/page.service.client.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+
+// injecting service into module
+@Injectable()
+
+export class PageService {
+
+  constructor() { }
+
+	pages = [
+	  { _id: "321", name: "Post 1", websiteId: "456", description: "Lorem" },
+	  { _id: "432", name: "Post 2", websiteId: "456", description: "Lorem" },
+	  { _id: "543", name: "Post 3", websiteId: "456", description: "Lorem" }
+	]
+
+
+	// adds the page parameter instance to the local pages array. The new page's websiteId is set to the websiteId parameter
+	createPage(websiteId : string, page : any){
+		page._id = Math.floor(Math.random() * 10000).toString();
+		page.websiteId = websiteId;
+		this.pages.push(page);
+		return page;
+	}
+
+	// retrieves the pages in local pages array whose websiteId matches the parameter websiteId
+	findPageByWebsiteId(websiteId : string) {
+		let result = [];
+		for(let i =0; i < this.pages.length;i++){
+			if(this.pages[i].websiteId === websiteId) {
+				result.push(this.pages[i]);
+			}
+		}
+
+		return result;
+	}
+
+	//  retrieves the page in local pages array whose _id matches the pageId parameter
+	findPageById(pageId : string) {
+		for(let i=0;i<this.pages.length;i++) {
+			if(this.pages[i]._id === pageId) {
+				return this.pages[i];
+			}
+		}
+	}
+
+	// updates the page in local pages array whose _id matches the pageId parameter
+	updatePage(pageId : string, page: any) {
+		let oldPage = this.findPageById(pageId);
+		const index = this.pages.indexOf(oldPage);
+		this.pages[index].name = page.name;
+		this.pages[index].description = page.description;
+	}
+
+	//  removes the page from local pages array whose _id matches the pageId parameter
+	deletePage(pageId : string) {
+		let oldPage = this.findPageById(pageId);
+		const index = this.pages.indexOf(oldPage);
+		this.pages.splice(index, 1);
+	}
+
+
+}
