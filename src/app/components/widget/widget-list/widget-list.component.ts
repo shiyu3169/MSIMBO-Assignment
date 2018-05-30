51b8c30c8590c28dc73a4d1fc67ef129d773e6ad
Shiyu Wang <shiyu3169@gmail.com>
2018-05-30 09:51:59 -0400

    widget list

--- src/app/components/widget/widget-list/widget-list.component.ts
+++ src/app/components/widget/widget-list/widget-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { WidgetService } from "../../../services/widget.service.client"
+import { Widget } from "../../../models/widget.model.client"
+import { ActivatedRoute } from "@angular/router"
+import { DomSanitizer } from '@angular/platform-browser'
 
 @Component({
   selector: 'app-widget-list',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WidgetListComponent implements OnInit {
 
-  constructor() { }
+  uid: string;
+  wid: string;
+  pid: string;
+  widgets: Widget[];
+
+  constructor(private widgetService: WidgetService, private activatedRoute: ActivatedRoute, private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
+  	this.activatedRoute.params.subscribe(params => {
+  		this.uid = params['uid'];
+  		this.wid = params['wid'];
+  		this.pid = params['pid'];
+  		this.widgets = this.widgetService.findWidgetsByPageId(this.pid);
+  	})
+  }
+
+  getYoutubeUrl(url){
+  	let embedUrl = "https://www.youtube.com/embed/";
+  	const parsedUrl = url.split('/');
+  	// tranfer video url into emebed video url
+  	embedUrl += parsedUrl[parsedUrl.length -1];
+
+  	// telling browser this src is safe
+  	return this.sanitizer.bypassSecurityTrustResourceUrl(embedUrl);
   }
 
 }
