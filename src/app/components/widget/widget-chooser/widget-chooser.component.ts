328ffb1b597328b2db958ff489ea4f891e9a5368
Shiyu Wang <shiyu3169@gmail.com>
2018-05-31 09:29:08 -0400

    a4 finished

--- src/app/components/widget/widget-chooser/widget-chooser.component.ts
+++ src/app/components/widget/widget-chooser/widget-chooser.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { WidgetService } from '../../../services/widget.service.client'
+import { Widget } from '../../../models/widget.model.client'
 
 @Component({
   selector: 'app-widget-chooser',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WidgetChooserComponent implements OnInit {
 
-  constructor() { }
+  uid: string;
+  wid: string;
+  pid: string;
+
+  constructor(private activatedRoute: ActivatedRoute, private widgetService: WidgetService, private router: Router) { }
 
   ngOnInit() {
+  	this.activatedRoute.params.subscribe(params=> {
+  		this.uid = params['uid'];
+  		this.wid = params['wid'];
+  		this.pid = params['pid'];
+  	})
   }
+  
+  create(type){
+  	const newWidget: Widget = {
+  		_id: '',
+  		widgetType: type,
+  		pageId: this.pid
+  	}
+  	this.widgetService.createWidget(this.pid, newWidget);
 
+  	const wgid = this.widgetService.widgets[this.widgetService.widgets.length - 1]._id;
+
+  	this.router.navigate(['user', this.uid, 'website', this.wid, 'page', this.pid, 'widget', wgid]);
+  }
 }
