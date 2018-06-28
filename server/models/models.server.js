155ee9e950726b90ea804d051be792d4e824cc71
Shiyu Wang <shiyu3169@gmail.com>
2018-06-28 09:47:18 -0400

    adding database

--- server/models/models.server.js
+++ server/models/models.server.js
@@ -4,7 +4,7 @@ if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
   var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
   var password = process.env.MLAB_PASSWORD_WEBDEV;
   connectionString = 'mongodb://' + username + ':' + password;
-  connectionString += '@ds129004.mlab.com:29004/heroku_gln9d69c'; // use yours
+  connectionString += '@ds129946.mlab.com:29946/heroku_z7t7l4w7'; // use yours
 }
 
 var mongoose = require("mongoose");
